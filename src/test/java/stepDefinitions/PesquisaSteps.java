dab429e64300b1b8b6ab9936bc70063bee007f28
RoninQA PC <ronierisoncosta@gmail.com>
2022-06-02 14:55:14 -0300

    Adicionando runner

---
+++ src/test/java/stepDefinitions/PesquisaSteps.java
@@ -0,0 +1,27 @@
+package stepDefinitions;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+public class PesquisaSteps {
+    @Dado("^que estou na pagina de pesquisa$")
+    public void que_estou_na_pagina_de_pesquisa() throws Throwable {
+
+    }
+
+    @Quando("^pesquisar por um produto produto <VALIDO>$")
+    public void pesquisar_por_um_produto_produto_VALIDO() throws Throwable {
+
+    }
+
+    @Entao("^retorna uma quantidade de produtos$")
+    public void retorna_uma_quantidade_de_produtos() throws Throwable {
+
+    }
+
+    @Entao("^aparece uma lista de produtos$")
+    public void aparece_uma_lista_de_produtos() throws Throwable {
+
+    }
+}
